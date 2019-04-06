f0d0a3b47c773d56e60806b7362bb1f866adf9c6
Your Name <you@example.com>
2019-04-05 22:08:44 -0700

    build-speed.sh: added

---
+++ amd/build-speed.sh
@@ -0,0 +1,11 @@
+if [[ -z $1 ]] ; then
+	echo "ERROR: PARAMETER 1  is empty"
+fi
+START1=`echo $SECONDS`
+echo Building using the command: $1 $2 $3 $4
+$1 $2 $3 $4
+time sleep 3
+END1=`echo $SECONDS`
+echo START1 / END1: $START1  / $END1
+DURATION=$(($END1 - $START1))
+echo Build duration: $DURATION
