e79bce2e47dfda3f9446d5fd999eada011a0d7b3
Your Name <you@example.com>
2019-04-06 12:49:55 -0700

    build-speed.sh: try using date command to measure duration

--- amd/build-speed.sh
+++ amd/build-speed.sh
@@ -2,11 +2,11 @@ if [[ -z $1 ]] ; then
 	echo "ERROR: PARAMETER 1  is empty"
 	exit 1
 fi
-START1=`echo $SECONDS`
+START1=`date +%s`
 echo Building using the command: $1 $2 $3 $4
 $1 $2 $3 $4
 time sleep 3
-END1=`echo $SECONDS`
+END1=`date +%s`
 echo START1 / END1: $START1  / $END1
 DURATION=$(($END1 - $START1))
 echo Build duration: $DURATION
